8b89ad6ec49a22d54870f45017636d0812e6149b
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-10-04 14:40:46 +0300

    complete task

---
+++ qt_h_!.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6 import uic
+
+
+class Calculator(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('calc.ui', self)
+        self.pushButtonEqual.clicked.connect(self.calculate)
+        self.pushButtonClear.clicked.connect(self.clear)
+
+    def calculate(self):
+        try:
+            expression = self.lineEdit.text()
+            result = eval(expression)
+            self.lineEdit.setText(str(result))  #
+        except ZeroDivisionError:
+            self.show_error("Ошибка: Деление на ноль.")
+        except Exception as e:
+            self.show_error(f"Ошибка: {str(e)}")
+
+    def clear(self):
+        self.lineEdit.clear()
+
+    def show_error(self, message):
+        QMessageBox.critical(self, 'Ошибка', message)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Calculator()
+    window.show()
+    sys.exit(app.exec())
