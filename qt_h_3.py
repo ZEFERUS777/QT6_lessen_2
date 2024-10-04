8fe614255fd25987ba4926affadfa18a704a1606
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-10-04 19:35:39 +0300

    I made the visual part of the program, but the functionality remains

---
+++ qt_h_3.py
@@ -0,0 +1,46 @@
+import sys
+
+from PyQt6.QtWidgets import QWidget, QApplication, QTimeEdit, QCalendarWidget, QHBoxLayout, QVBoxLayout, QLineEdit, \
+    QPushButton, QListWidget
+
+
+class SimplePlanner(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(300, 300, 600, 600)
+        self.setWindowTitle('Мини планировщик')
+        self.timeEdit = QTimeEdit(self)
+        self.timeEdit.setGeometry(100, 100, 200, 200)
+        self.timeEdit.setDisplayFormat('mm:ss')
+        self.timeEdit.move(0, 0)
+        self.timeEdit.resize(320, 25)
+        self.calendarWidget = QCalendarWidget(self)
+        self.calendarWidget.move(0, 30)
+        self.calendarWidget.resize(320, 400)
+        self.main_lay = QHBoxLayout(self)
+        self.left_lay = QVBoxLayout(self)
+        self.right_lay = QVBoxLayout(self)
+
+        self.left_lay.addWidget(self.timeEdit)
+        self.left_lay.addWidget(self.calendarWidget)
+
+        self.task_inp = QLineEdit(self)
+        self.task_inp.move(0, 425)
+        self.task_inp.resize(320, 25)
+        self.left_lay.addWidget(self.task_inp)
+
+        self.addEventBtn = QPushButton('Добавить событие', self)
+        self.addEventBtn.clicked.connect(self.addEvent)
+        self.addEventBtn.move(0, 460)
+        self.addEventBtn.resize(320, 25)
+
+        self.eventList = QListWidget(self)
+        self.eventList.move(330, 0)
+        self.eventList.resize(265, 485)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = SimplePlanner()
+    window.show()
+    sys.exit(app.exec())
