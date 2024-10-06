f66c4d91dc6355cbe70bba2aa344bca9e88121dc
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-10-06 19:36:46 +0300

    Добавил сортировку по дате и времени

--- qt_h_3.py
+++ qt_h_3.py
@@ -1,7 +1,7 @@
 import sys
-
 from PyQt6.QtWidgets import QWidget, QApplication, QTimeEdit, QCalendarWidget, QHBoxLayout, QVBoxLayout, QLineEdit, \
     QPushButton, QListWidget
+from PyQt6.QtCore import QDateTime
 
 
 class SimplePlanner(QWidget):
@@ -10,13 +10,9 @@ class SimplePlanner(QWidget):
         self.setGeometry(300, 300, 600, 500)
         self.setWindowTitle('Мини планировщик')
         self.timeEdit = QTimeEdit(self)
-        self.timeEdit.setGeometry(100, 100, 200, 200)
         self.timeEdit.setDisplayFormat('mm:ss')
-        self.timeEdit.move(0, 0)
-        self.timeEdit.resize(320, 25)
         self.calendarWidget = QCalendarWidget(self)
-        self.calendarWidget.move(0, 30)
-        self.calendarWidget.resize(320, 400)
+
         self.main_lay = QHBoxLayout(self)
         self.left_lay = QVBoxLayout(self)
         self.right_lay = QVBoxLayout(self)
@@ -25,23 +21,37 @@ class SimplePlanner(QWidget):
         self.left_lay.addWidget(self.calendarWidget)
 
         self.task_inp = QLineEdit(self)
-        self.task_inp.move(0, 425)
-        self.task_inp.resize(320, 25)
         self.left_lay.addWidget(self.task_inp)
 
         self.addEventBtn = QPushButton('Добавить событие', self)
         self.addEventBtn.clicked.connect(self.addEvent)
-        self.addEventBtn.move(0, 460)
-        self.addEventBtn.resize(320, 25)
+        self.left_lay.addWidget(self.addEventBtn)
 
         self.eventList = QListWidget(self)
-        self.eventList.move(330, 0)
-        self.eventList.resize(265, 485)
+        self.right_lay.addWidget(self.eventList)
+
+        self.main_lay.addLayout(self.left_lay)
+        self.main_lay.addLayout(self.right_lay)
+
+        self.events = []
 
     def addEvent(self):
-        selected_data = self.calendarWidget.selectedDate()
-        self.eventList.addItem(
-            f"{selected_data.toString('dd-MM-yyyy')} 00:{self.timeEdit.text()} - {self.task_inp.text()}")
+        selected_date = self.calendarWidget.selectedDate()
+        time_str = self.timeEdit.text()
+        task_str = self.task_inp.text()
+
+        event_time = QDateTime(selected_date.year(), selected_date.month(),
+                               selected_date.day(), int(time_str[:2]), int(time_str[3:]))
+
+        self.events.append((event_time, task_str))
+        self.events.sort(key=lambda x: x[0])
+
+        self.updateEventList()
+
+    def updateEventList(self):
+        self.eventList.clear()
+        for event_time, task_str in self.events:
+            self.eventList.addItem(f"{event_time.toString('dd-MM-yyyy hh:mm')} - {task_str}")
 
 
 if __name__ == '__main__':
