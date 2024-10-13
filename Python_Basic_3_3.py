14620a54aff6a3c13ef99c8d5cd44a7585cd6787
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-10-13 13:08:29 +0300

    выложил недоделанный класс

--- Python_Basic_3_3.py
+++ Python_Basic_3_3.py
@@ -1,5 +1,5 @@
 class Point:
-    def __init__(self, name, x, y):
+    def __init__(self, name: str, x: int, y: int):
         self.name = name
         self.x = x
         self.y = y
@@ -23,20 +23,40 @@ class Point:
     def get_coords(self):
         return tuple((self.x, self.y))
 
+    def __repr__(self):
+        return f'Point(\'{self.name}\', {self.x}, {self.y})'
 
-class ColoredPoint(Point):
-    def __init__(self, name, x, y, color=(0, 0, 0)):
-        self.color = tuple(color)
-        self.name = name
-        self.x = x
-        self.y = y
-        self.cord = tuple((x, y))
-        self.r = tuple(color)[0]
-        self.g = tuple(color)[1]
-        self.b = tuple(color)[2]
+    def __eq__(self, other):
+        return self.cord == other.cord and self.name == other.name and self.x == other.x and self.y == other.y
 
-    def get_color(self):
-        return self.color
+    def __ne__(self, other):
+        return not self == other
 
-    def __invert__(self):
-        return ColoredPoint(self.name, self.y, self.x, tuple((255 - self.r, 255 - self.g, 255 - self.b)))
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
+    def __ge__(self, other):
+        return self.x >= other.x and self.y >= other.y
+
+    def __lt__(self, other):
+        return self.x < other.x and self.y < other.y
+
+    def __le__(self, other):
+        return self.x <= other.x and self.y <= other.y
+
+p_A1 = Point('A', 1, 2)
+p_A2 = Point('A', 2, 1)
+p_B1 = Point('B', 2, 3)
+p_B2 = Point('B', 2, 3)
+
+print(p_A1 == p_A2, p_B1 == p_B2)
+print(p_A1 != p_A2, p_B1 != p_B2)
+print(p_A1 < p_A2, p_B1 > p_B2)
+print(p_A1 >= p_A2, p_B1 <= p_B2)
+print(max(p_A1, p_B2, p_A2, p_B2))
+print(min(p_A1, p_B2, p_A2, p_B2))
+
+points = [Point('A', 101, 1), Point('B', -1, 0),
+          Point('A', 11, 0), Point('A', 111, -11)]
+points.sort()
+print(', '.join(map(str, points)))
