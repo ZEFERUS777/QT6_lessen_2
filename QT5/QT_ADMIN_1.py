f84c3794308dc17459f3a6fd4160dcf025241b1e
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-10-25 13:20:49 +0300

    complete program

--- QT5/QT_ADMIN_1.py
+++ QT5/QT_ADMIN_1.py
@@ -35,7 +35,7 @@ class administrator_LMS(QMainWindow):
         self.save_all_units_btn.adjustSize()
         self.add_unit.resize(self.save_all_units_btn.width(), self.save_all_units_btn.height())
 
-        self.units_list = {}
+        self.units_list = []
 
         self.units_list_ui = QListWidget(self)
         self.units_list_ui.move(self.save_all_units_btn.x(),
@@ -44,6 +44,23 @@ class administrator_LMS(QMainWindow):
 
         self.add_unit.clicked.connect(self.add_unit_to_list)
         self.save_all_units_btn.clicked.connect(self.save_all_units)
+
+    def add_unit_to_list(self):
+        try:
+            self.units_list_ui.addItem(f'{self.unit_name_input.text()}: {self.name_mug_input.text()}')
+            self.units_list_ui.resize(self.save_all_units_btn.width(), self.units_list_ui.height() + 15)
+            self.units_list.append(f'{self.unit_name_input.text()} {self.name_mug_input.text()}')
+        except ValueError:
+            print('Необходимо ввести имя ученика и название кружка')
+
+    def save_all_units(self):
+        file_name, ok_pressed = QInputDialog.getText(self, 'Сохранить', 'Введите имя файла')
+        if ok_pressed:
+            with open(file_name + '.txt', 'w', encoding='utf-8') as file:
+                for unit in self.units_list:
+                    unit = unit.split(' ')
+                    file.write(f'Имя ученика: {unit[0]} Название кружка: {unit[1]}\n')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
