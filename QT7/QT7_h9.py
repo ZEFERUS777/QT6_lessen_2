cfefba524fe29320cc42fe18b2e4a8aa09c1f905
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-11-15 20:19:27 +0300

    complete task

--- QT7/QT7_h9.py
+++ QT7/QT7_h9.py
@@ -4,3 +4,11 @@ conn = sqlite3.connect(input())
 
 curr = conn.cursor()
 
+curr.execute("""
+SELECT DISTINCT genres.title FROM films 
+JOIN genres ON films.genre = genres.id WHERE films.year = 2010 OR films.year = 2011;""")
+
+result = curr.fetchall()
+
+for i in result:
+    print(i[0])
